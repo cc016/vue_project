5129decc7cdfd9c910f753f60a084511e3b52153
西竹 <2716728837@qq.com>
2021-07-14 16:11:05 +0800

    第三次提交，完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import store from './store'
 // 导入配置表  以便读取element ui 包含的标签……
 import './plugins/element.js'
 
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
 // 导入全局样式表
 import './assets/css/global.css'
 
@@ -13,6 +16,13 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+    // axios请求拦截  登录成功后拦截  
+    // 需要授权的API,所以把token保存到请求头里
+axios.interceptors.request.use(config => {
+        // 为请求头对象，添加Token 验证的 Authorization 字段
+        config.headers.Authorization = window.sessionStorage.getItem('token')
+        return config
+    })
     // 把axios 挂载到原型对象上  这样的话 每一个vue组件  可以通过this直接访问$http 从而发送axios请求
 Vue.prototype.$http = axios
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,13 +1,58 @@
 import Vue from 'vue'
 // 使用element.js的基本标签
-import { Button, Form, FormItem, Input } from 'element-ui'
-// 导入消息弹框
-import { Message } from 'element-ui'
+import {
+    Button,
+    Form,
+    FormItem,
+    Input,
+    Message,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItemGroup,
+    MenuItem,
+    Breadcrumb,
+    BreadcrumbItem,
+    Card,
+    Row,
+    Col,
+    Table,
+    TableColumn,
+    Switch,
+    Tooltip,
+    Pagination,
+    Dialog,
+    MessageBox
+} from 'element-ui'
+
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
-
-// Message 需要挂载到vue原型对象上  $message/$http是自定义的属性  this就指代了Vue原型对象 所以可以直接this.$message
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+    // Message 需要挂载到vue原型对象上  $message/$http是自定义的属性  this就指代了Vue原型对象 所以可以直接this.$message
 Vue.prototype.$message = Message
+
+Vue.prototype.$confirm = MessageBox.confirm
